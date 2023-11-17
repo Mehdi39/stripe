8a1be554d08811a2c15464280fbb4bfc4bb5b5a6
MD. Jahdi Hossain Mridha <jahid4k@gmail.com>
2023-11-17 22:39:52 +0600

    office closed

--- src/screen/ApplePay.jsx
+++ src/screen/ApplePay.jsx
@@ -1,11 +1,19 @@
-import React, {useEffect} from 'react'
+/**
+ * 1. APPLE PAY DOMAIN REGISTERED ON STRIPE
+ * 2. WEBSITE ON HTTPS
+ * 3. CARDS SETUP ON APPLE PAY WALLET
+ */
+
+import React, { useEffect, useState } from 'react'
 import StatusMessage, { useMessages } from '../components/StatusMessages';
-import { useStripe, useElements } from '@stripe/react-stripe-js'
+import { PaymentRequestButtonElement, useStripe, useElements } from '@stripe/react-stripe-js'
+import { Result } from 'postcss';
 
 const ApplePay = () => {
     const stripe = useStripe();
     const elements = useElements()
     const [messages, addMessage] = useMessages();
+    const [paymentRequest, setPaymentRequest] = useState(null);
 
     useEffect(() => {
         if(!stripe || !elements) return
@@ -20,12 +28,19 @@ const ApplePay = () => {
                 amount: 1999,
             }
         })
+
+        pr.canMakePayment().then((result) => {
+            if (result) {
+                // show some button on the page
+                setPaymentRequest(pr)
+            }
+        })
     }, [stripe, elements, addMessage])
 
     return (
         <div className='h-screen bg-gray-200'>
             <h1>Apple Pay</h1>
-
+            {paymentRequest && <PaymentRequestButtonElement options={{paymentRequest}}/>}
             <StatusMessage messages={messages} />
         </div>
     )
